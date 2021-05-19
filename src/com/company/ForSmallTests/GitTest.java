d93352ab51e8bed2c903284e96e7046fa974ac77
VolodinAnton <73581346+VolodinAnton@users.noreply.github.com>
2021-05-20 09:31:15 +1000

    Add GitTest

---
+++ src/com/company/ForSmallTests/GitTest.java
@@ -0,0 +1,11 @@
+package com.company.ForSmallTests;
+// Синий файл - данный файл явл модифицированным, те изменен, но изменения не залиты на гитхаб
+// Зеленый файл = данный файл добавлен в проект, но не залит еще на ГитХаб
+
+
+public class GitTest {
+    public void print(String a) {
+        System.out.println(a);
+        System.out.println();
+    }
+}
--- src/com/company/ForSmallTests/Main.java
+++ src/com/company/ForSmallTests/Main.java
@@ -15,5 +15,6 @@ public class Main {
       int c = 5;
         System.out.println(a);
         System.out.println(b/c);
+
     }
 }
