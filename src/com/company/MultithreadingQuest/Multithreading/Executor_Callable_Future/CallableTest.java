fe52b2339c2e9f825b3670f0465eb674f3adacec
VolodinAnton <73581346+VolodinAnton@users.noreply.github.com>
2021-06-03 17:19:24 +1000

    Add Pacage Executor_Callable_Future

---
+++ src/com/company/MultithreadingQuest/Multithreading/Executor_Callable_Future/CallableTest.java
@@ -0,0 +1,11 @@
+package com.company.MultithreadingQuest.Multithreading.Executor_Callable_Future;
+
+import java.util.concurrent.Callable;
+
+public class CallableTest {
+    public static void main(String[] args) {
+
+    }
+}
+
+
